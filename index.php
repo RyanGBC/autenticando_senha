6547d7af0cb2a076b5b3a6c89ae50c44ea42a6ce
RyanGBC <ryangabrj@gmail.com>
2025-03-12 16:35:32 -0300

    feat: editando o login

--- index.php
+++ index.php
@@ -16,18 +16,57 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="style.css"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
     <h1>Site de LOGIN em PHP</h1>
-    Cadastrar Senha <br><br>
-    <main id="caixacentral">
-    <form action="" method="POST"> 
-        <input type="text" name="email" placeholder="email" id="cxinputs"><br><br>
-        <input type="text" name="senha" placeholder="senha" id="cxinputs"><br><br>
-        <button type="submit" name="login" id="cxlogin">Cadastrar</button>
-    </form><br><br>
+    <form action="" method="POST">
+  <div class="row justify-content-center">
+    <div class="col-md-6 col-lg-4">
+      <form action="" method="POST">
+      <form action="" method="POST">
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Endereço de Email</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text">Nos nunca compartilhamos seu email com terceiros</div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Senha</label>
+    <input type="password" class="form-control" id="exampleInputPassword1">
+  </div>
+  <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Lembrar me</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Cadastrar</button>
+</form>
+      </form>
+    </div>
+  </div>
+</div>
     <a href="login.php">Já tenho uma conta</a>
     </main>
 </body>
 </html>
+
+<style type="text/css" media="screen">
+    a {
+        text-align: center;
+        display: block;
+        margin-top: 10px;
+    }
+    h1 {
+    text-align: center;
+    margin-top: 0px;
+    color: #007bff;
+    background-color: #333;
+    padding: 50px 0;
+    margin-bottom: 150px;
+    width: 100%;
+    margin-left: 0;
+    margin-right: 0;
+    box-sizing: border-box; 
+}
+</style>
--- login.php
+++ login.php
@@ -28,7 +28,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="style.css"/>
 </head>
 <body>
     <h1>LOGUE NO SITE DE PHP</h1>
@@ -37,5 +36,57 @@
         <input type="password" name="senha" placeholder="senha" id="cxinputs"><br><br>
         <button type="submit" name="login" id="cxlogin">Logar</button>
     </form>
+        <!-- From Uiverse.io by satyamchaudharydev --> 
+    <button class="button" data-text="Awesome">
+    <span class="actual-text">&nbsp;THE BEST OF THE WORLD &nbsp;</span>
+    <span aria-hidden="true" class="hover-text">&nbsp;THE BEST OF THE WORLD &nbsp;</span>
+    </button>
 </body>
 </html>
+
+<style>
+    /* From Uiverse.io by satyamchaudharydev */ 
+/* === removing default button style ===*/
+.button {
+  margin: 0;
+  height: auto;
+  background: transparent;
+  padding: 0;
+  border: none;
+  cursor: pointer;
+}
+
+/* button styling */
+.button {
+  --border-right: 6px;
+  --text-stroke-color: rgba(255,255,255,0.6);
+  --animation-color:rgb(55, 215, 255);
+  --fs-size: 2em;
+  letter-spacing: 3px;
+  text-decoration: none;
+  font-size: var(--fs-size);
+  font-family: "Arial";
+  position: relative;
+  text-transform: uppercase;
+  color: transparent;
+  -webkit-text-stroke: 1px var(--text-stroke-color);
+}
+/* this is the text, when you hover on button */
+.hover-text {
+  position: absolute;
+  box-sizing: border-box;
+  content: attr(data-text);
+  color: var(--animation-color);
+  width: 0%;
+  inset: 0;
+  border-right: var(--border-right) solid var(--animation-color);
+  overflow: hidden;
+  transition: 0.5s;
+  -webkit-text-stroke: 1px var(--animation-color);
+}
+/* hover */
+.button:hover .hover-text {
+  width: 100%;
+  filter: drop-shadow(0 0 23px var(--animation-color))
+}
+</style>
